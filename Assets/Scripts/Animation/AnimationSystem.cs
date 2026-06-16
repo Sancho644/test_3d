382d0b6f7a28373f33823e3d9d48ef7b860e1132
Alexandr <rfst@mail.ru>
2026-06-16 20:16:07 +0700

    edit play merge animation

--- Assets/Scripts/Animation/AnimationSystem.cs
+++ Assets/Scripts/Animation/AnimationSystem.cs
@@ -8,10 +8,10 @@ namespace Animation
     public class AnimationSystem : MonoBehaviour
     {
         [SerializeField] private float baseDuration = 0.5f;
+        [SerializeField] private float peakHeight = 2f;
 
         public IEnumerator PlayMerge(MergeResult result, float speedMultiplier)
         {
-            var duration = baseDuration / speedMultiplier;
             var placedStackData = result.PlacedStackData;
             var targetStackData = result.TargetStackData;
 
@@ -24,20 +24,31 @@ namespace Animation
             var placedView = placedStackData.View;
             var targetView = targetStackData.View;
 
-            if (targetView.HexList.Count == 0)
+            if (targetView.HexList.Count == 0 || placedView.HexList.Count == 0)
                 yield break;
 
-            var center = targetView.HexList[^1].gameObject.transform.position;
             var hexHeight = targetView.HexList[0].Height;
-            
-            center = new Vector3(center.x, center.y + hexHeight, center.z);
-            
-            foreach (var hex in placedView.HexList)
-            {
-                hex.transform.DOMove(center, duration);
-            }
+            var baseTargetPos = targetView.HexList[^1].transform.position + Vector3.up * hexHeight;
 
-            yield return new WaitForSeconds(duration);
+            for (int i = 0; i < placedView.HexList.Count; i++)
+            {
+                var hex = placedView.HexList[i];
+                if (hex == null)
+                    continue;
+
+                var startPos = hex.transform.position;
+                var targetPos = baseTargetPos + Vector3.up * (hexHeight * i);
+                var peakPos = Vector3.Lerp(startPos, targetPos, 0.5f) + Vector3.up * peakHeight;
+
+                var halfDuration = baseDuration / speedMultiplier / 2f;
+
+                var seq = DOTween.Sequence();
+                seq.Append(hex.transform.DOMove(peakPos, halfDuration).SetEase(Ease.OutQuad));
+                seq.Join(hex.transform.DORotate(new Vector3(0f, 0f, 180f), halfDuration * 2f).SetEase(Ease.InOutQuad));
+                seq.Append(hex.transform.DOMove(targetPos, halfDuration).SetEase(Ease.InQuad));
+
+                yield return seq.WaitForCompletion();
+            }
         }
     }
 }
